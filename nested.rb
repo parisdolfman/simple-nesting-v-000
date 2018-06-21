22878a3982bcf2d52946bd9516c2c250a64e561e
Paris Dolfman <parisdolfman@gmail.com>
2018-06-21 18:03:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,13 @@ def adding_matz
         }
      }
 
+     programmer_hash[:yukihiro_matsumoto] = {
+         known_for: "Ruby",
+         languages:
+           ["LISP", "C"]
+         }
+         
+       programmer_hash
 
 end
 
