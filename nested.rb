d855c261cb651ebe9219905abefefb65ed733e91
Paris Dolfman <parisdolfman@gmail.com>
2018-06-21 17:24:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,8 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		lang = programmer_hash[:dennis_ritchie][:languages]
-    puts lang.to_s
+		programmer_hash[:dennis_ritchie][:languages]
+    
 end
 
 def adding_matz
